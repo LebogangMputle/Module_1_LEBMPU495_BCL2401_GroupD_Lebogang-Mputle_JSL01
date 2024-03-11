1ea6250d78325c4f2a30a44fd3bc1fd76a6df5c0
LebogangMputle <lebozikalala@gmail.com>
2024-03-11 14:04:38 +0200

    adding the validator

--- script.js
+++ script.js
@@ -1,12 +1,27 @@
-function validateSyntax() {
-    let input = document.getElementById('petInput').value;
-    // Validation logic goes here
-    let result = ''; // Placeholder for validation result
+/*function validateSyntax() {
+    let input = document.getElementById("petInput").value;
+    let result = ""; // Placeholder for validation result
+  
+    // Check if input starts with 'pet' and followed by alphanumeric characters
+    if (/^pet\d+[a-zA-Z]+$/.test(input)) {
+      result = "Valid Syntax";
+    } else {
+      result = "Invalid Syntax";
+    }
+  
+    document.getElementById("result").innerText = result;
+  }*/
 
-    // TODO: Write your validation logic here
-        // Check if input starts with 'pet_' and followed by alphanumeric characters
-
-            document.getElementById('result').innerText = result;
-}
-
-
+  function validateSyntax() {
+    let input = document.getElementById("petInput").value;
+    let result = ""; // Placeholder for validation result
+  
+    // Check if input starts with 'pet' and followed by alphanumeric characters
+    if (/^pet_\d+[a-zA-Z]+$/.test(input)) {
+      result = "Valid Syntax ";
+    } else {
+      result = "Invalid Syntax";
+    }
+  
+    document.getElementById("result").innerText = result;
+  }
