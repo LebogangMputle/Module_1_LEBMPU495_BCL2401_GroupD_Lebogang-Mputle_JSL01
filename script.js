b5571df455d508810b867761c77dd11f191e25d2
LebogangMputle <lebozikalala@gmail.com>
2024-03-14 09:58:27 +0200

    adding comments

--- script.js
+++ script.js
@@ -1,27 +1,15 @@
-/*function validateSyntax() {
-    let input = document.getElementById("petInput").value;
-    let result = ""; // Placeholder for validation result
-  
-    // Check if input starts with 'pet' and followed by alphanumeric characters
-    if (/^pet\d+[a-zA-Z]+$/.test(input)) {
-      result = "Valid Syntax";
-    } else {
-      result = "Invalid Syntax";
-    }
-  
-    document.getElementById("result").innerText = result;
-  }*/
-
+ // Get input value from an HTML element with id "petInput"
   function validateSyntax() {
     let input = document.getElementById("petInput").value;
     let result = ""; // Placeholder for validation result
   
-    // Check if input starts with 'pet' and followed by alphanumeric characters
+    // Check if input starts with 'pet_' and followed by (d+ which is the numeric symbol) then alphabetic characters ranging from a-z in small and capital letters.
     if (/^pet_\d+[a-zA-Z]+$/.test(input)) {
-      result = "Valid Syntax 🟢";
+      result = "Valid Syntax 🟢"; //if the correct input is inserted, this is the resulting message with a green dot emoji.
     } else {
-      result = "Invalid Syntax 🔴";
+      result = "Invalid Syntax 🔴";  //or else the incorrect input is inserted, this is the resulting message with a red dot emoji.
     }
   
+    // Display validation result on HTML element with id "result"
     document.getElementById("result").innerText = result;
   }
